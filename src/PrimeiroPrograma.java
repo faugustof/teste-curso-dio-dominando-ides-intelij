3ceecff8d5b7d810a2aa49f0ad6d8aad6556f800
faugustof <faugustof@gmail.com>
2022-10-12 07:35:32 -0300

    first commit.

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        /*int a =5;
+        int b =3;
+        System.out.printf("Olá Mundo " + (a+b));
+    }*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
